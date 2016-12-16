5a07282f87896e60ea3c39477c8f00e936e662ce
Trevor Vaughan <tvaughan@onyxpoint.com>
2016-12-16 15:55:35 -0500

    (SIMP-1695) Update for Puppet 4
    * Updated for Puppet 4
    * Renamed logrotate::add to logrotate::rule to be less verb-y
    
    SIMP-1695 #comment Updated across the board
    SIMP-2170 #close

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 describe 'logrotate' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) { facts }
 
-  it { is_expected.to create_class('logrotate') }
-  it { is_expected.to create_file('/etc/logrotate.conf').with_content(/weekly/) }
-  it { is_expected.to create_file('/etc/logrotate.d').with_ensure('directory') }
-  it { is_expected.to contain_package('logrotate').with_ensure('latest') }
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to create_class('logrotate') }
+        it { is_expected.to create_file('/etc/logrotate.conf').with_content(/weekly/) }
+        it { is_expected.to create_file('/etc/logrotate.d').with_ensure('directory') }
+        it { is_expected.to contain_package('logrotate').with_ensure('latest') }
+      end
+    end
+  end
 end
